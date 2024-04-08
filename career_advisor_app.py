cf354bb9f6214fe561da53eccedbc0175aac60b6
Tyler Misciagna <tmisciagna@student.saintaug.nsw.edu.au>
2024-04-09 09:26:34 +1000

    Testing github repository

--- career_advisor_app.py
+++ career_advisor_app.py
@@ -1,2 +1,15 @@
-import tk
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import os
 
+# Establishes Tkinter window properties
+career_app = tk.Tk()
+career_app.geometry(f"{1150}x{900}")
+career_app.title("Career Advisor App")
+career_app.resizable(False, False)
+
+mainframe = ttk.Frame(career_app, padding = "3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+career_app.mainloop
