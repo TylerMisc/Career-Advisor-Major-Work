83e0b937983b98082d1d66e1d979dcd3039b8499
Tyler Misciagna <tmisciagna@student.saintaug.nsw.edu.au>
2024-04-10 09:42:18 +1000

    Created the dropdown menus for HSC subject selection

--- career_advisor_app.py
+++ career_advisor_app.py
@@ -1,15 +1,31 @@
-from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-import os
 
-# Establishes Tkinter window properties
 career_app = tk.Tk()
-career_app.geometry(f"{1150}x{900}")
 career_app.title("Career Advisor App")
-career_app.resizable(False, False)
+career_app.geometry("400x400")
 
-mainframe = ttk.Frame(career_app, padding = "3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+# Subject categories, to be ammended 
+subject_categories = {
+    "English": ["English"],
+    "Mathematics": ["Mathematics"],
+    "Subject 1": ["Physics", "Chemistry", "Biology"],
+    "Subject 2": ["History", "Geography"],
+    "Subject 3": ["Design and Technology", "Information Technology"],
+    "Subject 4": ["French", "Spanish", "German"]
+    }
 
-career_app.mainloop
+subject_vars = []
+subject_menus = []
+
+# Dropdown menu for each subject
+for category, subjects in subject_categories.items():
+    label = ttk.Label(career_app, text=f"Select {category}:")
+    label.pack(pady=5)
+    var = tk.StringVar()
+    subject_vars.append(var)
+    menu = ttk.Combobox(career_app, values=subjects, textvariable=var)
+    menu.pack(pady=5)
+    subject_menus.append(menu)
+
+career_app.mainloop()
